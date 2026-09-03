19dc0ec40f4d7419e5f7a051efb2e69eaf13e777
ROG <kmergameing@gmail.com>
2026-09-03 19:52:37 +0700

    Add Google Search Console verification

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 
 export const metadata = {
+  verification: { google: 'FNYIdc1V-dZ6sT4v1k6nb1Qa9Dp9yb3zcO472YoQp3U' },
   title: 'MeowShop - Game Top Up Cambodia',
   description: 'MeowShop - Fast and easy game top up in Cambodia. Top up diamonds and game credits quickly and securely.',
   icons: {
